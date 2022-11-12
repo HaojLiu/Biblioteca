3df3ff35abfc13f898291cf3410e66dfa6bb0c1a
Haojie Liu <HAOJIE.LIU@local.ittcesena.it>
2022-11-12 09:27:14 +0100

    Creazione classe e aggiunta attributi

---
+++ BibliotecaScolastica/Biblioteca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaScolastica
+{
+    internal class Biblioteca
+    {
+        public string nome;
+        public string indirizzo;
+        public double orariodiapertura;
+        public double orariodichiusura;
+        public string[,] elencolibri = new string[1000,5]; //1000 libri, ognuno con 5 attributi
+        private int indice = 0; //indice usato solo per matrice 
+    }
+}
