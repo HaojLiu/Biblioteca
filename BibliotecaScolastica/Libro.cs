82c5491e938d01a331493913fc5f730b367b115c
Haojie Liu <HAOJIE.LIU@local.ittcesena.it>
2022-11-12 08:53:38 +0100

    Rifatto metodi get e set

--- BibliotecaScolastica/Libro.cs
+++ BibliotecaScolastica/Libro.cs
@@ -8,5 +8,24 @@ namespace BibliotecaScolastica
 {
     internal class Libro
     {
+        public string autore;
+        public string titolo;
+        public DateTime annodipubblicazione;
+        public string editore;
+        public int numerodipagine;
+
+        public string getAutore() { return autore; }
+        public string getTitolo() { return titolo; }
+        public DateTime getAnnodipubblicazione() { return annodipubblicazione; }
+        public string getEditore() { return editore; }
+        public int getNumerodipagine() { return numerodipagine; }
+
+        public void setAutore(string autore) { this.autore = autore; }
+        public void setTitolo(string titolo) { this.titolo = titolo; }
+        public void setAnnodipubblicaizone(DateTime annodipubblicazione) { this.annodipubblicazione = annodipubblicazione; }
+        public void setEditore(string editore) { this.editore = editore; }
+        public void setNumedidipagine(int numerodipagine) { this.numerodipagine = numerodipagine; }
+
+
     }
 }
