103fac9cd6ce9e54fa74d789e95cb99654d1f495
Haojie Liu <HAOJIE.LIU@local.ittcesena.it>
2022-11-12 09:02:29 +0100

    Aggiunto tutti metodi

--- BibliotecaScolastica/Libro.cs
+++ BibliotecaScolastica/Libro.cs
@@ -10,22 +10,36 @@ namespace BibliotecaScolastica
     {
         public string autore;
         public string titolo;
-        public DateTime annodipubblicazione;
+        public int annodipubblicazione;
         public string editore;
         public int numerodipagine;
 
         public string getAutore() { return autore; }
         public string getTitolo() { return titolo; }
-        public DateTime getAnnodipubblicazione() { return annodipubblicazione; }
+        public int getAnnodipubblicazione() { return annodipubblicazione; }
         public string getEditore() { return editore; }
         public int getNumerodipagine() { return numerodipagine; }
 
         public void setAutore(string autore) { this.autore = autore; }
         public void setTitolo(string titolo) { this.titolo = titolo; }
-        public void setAnnodipubblicaizone(DateTime annodipubblicazione) { this.annodipubblicazione = annodipubblicazione; }
+        public void setAnnodipubblicaizone(int annodipubblicazione) { this.annodipubblicazione = annodipubblicazione; }
         public void setEditore(string editore) { this.editore = editore; }
         public void setNumedidipagine(int numerodipagine) { this.numerodipagine = numerodipagine; }
 
+        public string toString()
+        {
+            return "Autore:\t\t" + getAutore() + "\nTitolo:\t\t" + getTitolo() + "\nAnno di pubblicazione:\t" + getAnnodipubblicazione().ToString() +
+                "\nEditore:\t\t" + getEditore() + "\nNumero di pagine:\t" + getNumerodipagine();
+        }
 
+        public string readingTime()
+        {
+            if (numerodipagine < 100)
+                return "circa un'ora";
+            else if (numerodipagine >= 100 || numerodipagine <= 200)
+                return "circa 2 ore";
+            else if (numerodipagine > 200)
+                return "maggiore di 2 ore";
+        }
     }
 }
